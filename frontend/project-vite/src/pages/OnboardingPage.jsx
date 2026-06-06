f9af73dccfda765790041ccc562b1401ee989902
Anoushka <koleyanoushka27@gmail.com>
2026-06-06 12:09:30 +0530

    new commit

--- frontend/project-vite/src/pages/OnboardingPage.jsx
+++ frontend/project-vite/src/pages/OnboardingPage.jsx
@@ -3,7 +3,7 @@ import useAuthUser from "../hooks/useAuthUser";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { completeOnboarding } from "../lib/api";
-import { LoaderIcon, MapPinIcon, ShipWheelIcon, ShuffleIcon ,CameraIcon} from "lucide-react";
+import { LoaderIcon,  ShipWheelIcon, ShuffleIcon ,CameraIcon} from "lucide-react";
 import { LANGUAGES,LOCATIONS } from "../constants";
 
 const OnboardingPage = () => {
